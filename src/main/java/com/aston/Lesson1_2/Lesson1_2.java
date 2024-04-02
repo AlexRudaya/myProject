d7ea93b8be15431cea0e819880c9f280af757fdb
AlexRudaya <alexandra.rudaya88@gmail.com>
2024-04-02 16:55:55 +0300

    test

--- src/main/java/com/aston/Lesson1_2/Lesson1_2.java
+++ src/main/java/com/aston/Lesson1_2/Lesson1_2.java
@@ -16,12 +16,15 @@ public class Lesson1_2 {
 //        changedMassive();
 //        longMassive();
 //        increasedMassive();
-//          twoDimensionalMass();
+//         twoDimensionalMass();
 //        oneDimensionalMass(5,8);
 
     }
 
     //  №1 task
+//    1. Создайте метод printThreeWords(), который при вызове
+//    должен отпечатать в столбец три слова: Orange, Banana,
+//    Apple.
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
     }
@@ -39,11 +42,18 @@ public class Lesson1_2 {
     }
 
     //  №3 task
+//    3. Создайте метод printColor() в теле которого задайте int
+//    переменную value и инициализируйте ее любым значением.
+//    Если value меньше 0 (0 включительно), то в консоль метод
+//    должен вывести сообщение “Красный”, если лежит в
+//    пределах от 0 (0 исключительно) до 100 (100 включительно),
+//    то “Желтый”, если больше 100 (100 исключительно) -
+//            “Зеленый”;
     public static void printColor() {
         int value = 100;
         if (value <= 0) {
             System.out.println("Красный");
-        } else if (value > 0 & value <= 100) {
+        } else if (value <= 100) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
@@ -63,6 +73,10 @@ public class Lesson1_2 {
 
 
     //  №5 task
+//    5. Напишите метод, принимающий на вход два целых числа и
+//    проверяющий, что их сумма лежит в пределах от 10 до 20
+//            (включительно), если да – вернуть true, в противном случае –
+//            false.
     public static boolean compareSumm(int a, int b) {
         return ((a + b) >= 10 & (a + b) <= 20);
 //        {
@@ -73,6 +87,10 @@ public class Lesson1_2 {
     }
 
     //    №6 task
+//    6. Напишите метод, которому в качестве параметра передается
+//    целое число, метод должен напечатать в консоль,
+//    положительное ли число передали или отрицательное.
+//    Замечание: ноль считаем положительным числом.
     public static void checkPositive(int a) {
         if (a >= 0) {
             System.out.println("Число " + a + " положительное");
@@ -82,6 +100,9 @@ public class Lesson1_2 {
     }
 
     //    №7 task
+//    Напишите метод, которому в качестве параметра передается
+//    целое число. Метод должен вернуть true, если число
+//    отрицательное, и вернуть false если положительное.
     public static boolean checkNumber(int a) {
         if (a < 0) {
             return true;
@@ -91,6 +112,9 @@ public class Lesson1_2 {
     }
 
     //    №8 task
+//    8. Напишите метод, которому в качестве аргументов
+//    передается строка и число, метод должен отпечатать в
+//    консоль указанную строку, указанное количество раз;
     public static void printString(String s, int a) {
         while (a > 0) {
             System.out.println(s);
@@ -100,11 +124,18 @@ public class Lesson1_2 {
 
 
     // №9 task
+//    9. Напишите метод, который определяет, является ли год
+//    високосным, и возвращает boolean (високосный - true, не
+//            високосный - false). Каждый 4-й год является високосным,
+//    кроме каждого 100-го, при этом каждый 400-й – високосный.
     public static boolean leapYear(int a) {
         return (((a % 4 == 0) & (a % 100 != 0)) | (a % 400 == 0));
     }
 
     // №10 task
+//    10. Задать целочисленный массив, состоящий из элементов 0
+//    и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и
+//    условия заменить 0 на 1, 1 на 0;
     public static void changedMassive() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1};
         System.out.println("Исходный массив " + Arrays.toString(arr));
@@ -119,6 +150,9 @@ public class Lesson1_2 {
     }
 
     //      №11 task
+//    11. Задать пустой целочисленный массив длиной 100. С
+//    помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ...
+//            100;
     public static void longMassive() {
         int[] arr = new int[100];
         for (int i = 1; i < 101; i++) {
@@ -128,6 +162,8 @@ public class Lesson1_2 {
     }
 
     // №12 task
+//    12. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по
+//    нему циклом, и числа меньшие 6 умножить на 2;
     public static void increasedMassive() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
@@ -139,6 +175,13 @@ public class Lesson1_2 {
     }
 
     //№13 task
+//    13. Создать квадратный двумерный целочисленный массив
+//            (количество строк и столбцов одинаковое), и с помощью цикла
+//(-ов) заполнить его диагональные элементы единицами
+//            (можно только одну из диагоналей, если обе сложно).
+//    Определить элементы одной из диагоналей можно по
+//    следующему принципу: индексы таких элементов равны, то
+//    есть [0][0], [1][1], [2][2], ..., [n][n];
     public static void twoDimensionalMass() {
         int n = 5;
         int i,j;
@@ -163,6 +206,9 @@ public class Lesson1_2 {
     }
 
     // №14 task
+//    14. Написать метод, принимающий на вход два аргумента: len и
+//    initialValue, и возвращающий одномерный массив типа int
+//    длиной len, каждая ячейка которого равна initialValue.
     public static void oneDimensionalMass(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < arr.length; i++) {
