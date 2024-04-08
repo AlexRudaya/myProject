c8fd9bf085dbbbbc32b0b1ee19a746050142c3ee
AlexRudaya <alexandra.rudaya88@gmail.com>
2024-04-08 21:39:56 +0300

    task for exceptions

--- src/main/java/com/aston/Lesson_5/Array.java
+++ src/main/java/com/aston/Lesson_5/Array.java
@@ -1,27 +1,58 @@
 package com.aston.Lesson_5;
 
+/** Напишите метод, на вход которого подаётся двумерный строковый
+ массив размером 4х4. При подаче массива другого размера
+ необходимо бросить исключение MyArraySizeException.
+ 2. Далее метод должен пройтись по всем элементам массива,
+ преобразовать в int и просуммировать. Если в каком-то элементе
+ массива преобразование не удалось (например, в ячейке лежит
+ символ или текст вместо числа), должно быть брошено исключение
+ MyArrayDataException с детализацией, в какой именно ячейке лежат
+ неверные данные.
+ 3. В методе main() вызвать полученный метод, обработать возможные
+ исключения MyArraySizeException и MyArrayDataException и вывести
+ результат расчета.*/
+
 public class Array {
     public static void main(String args[]) {
-        String[][] a = {{"5", "7", "3","5"}, {"5", "7", "3", "3"},  {"5", "7", "3", "17"},{"5", "7", "3", "17"}};
-        String[][] b = {{"5", "7", "3","5"},  {"5", "7", "3", "17"},{"5", "7", "3", "17"}};
-        try {exceptionTest(a);exceptionTest(b);}
-        catch (MyArraySizeException e) {
-            //     e.printStackTrace();
+        String[][] a = {{"5", "33", "3", "5"}, {"5", "7", "6", "3"}, {"5", "7", "3", "17"}, {"5", "7", "3", "17"}};
+        String[][] b = {{"5", "33", "3", "5"}, {"5", "not a number", "6", "3"}, {"5", "7", "3", "17"}, {"5", "7", "3", "17"}};
+        String[][] c = {{"5", "33", "3", "5"}, {"5", "7", "3", "17"}, {"5", "7", "3", "17"}};
+        String[][] d = {{"5", "33", "3", "5"}, {"5", "6", "6", "3"}, {"5", "7", "3", "17"}, {"5", "7", "3"}};
+
+
+        try {
+            exceptionTest(a);
+        } catch (MyArrayDataException | MyArraySizeException e) {
             System.out.println(e);
         }
+
+
     }
 
-    public static void exceptionTest(String[][] arr) throws MyArraySizeException {
-        if (arr.length!=4)
-        {throw new MyArraySizeException("Incorrect amount of strings");}
-        for (int i=0;i< arr.length;i++){
-            if (arr[i].length!=4 )  {throw new MyArraySizeException("Incorrect amount of columns");
-            };
-            for (int j=0; j< arr[i].length;j++){
+    public static void exceptionTest(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        double sum = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException("Incorrect amount of strings");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException("Incorrect amount of columns");
+            }
+            ;
+
+            for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
+                try {
+                    sum = sum + Double.parseDouble(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j, arr[i][j]);
+                }
             }
             System.out.println();
         }
-
+        System.out.println("Sum of all cells= " + sum);
     }
+
+
 }
