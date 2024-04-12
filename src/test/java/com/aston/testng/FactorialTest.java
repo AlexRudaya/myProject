36dcf8cc5d2e9d3fce2fcc8c9c1c376b85a22731
AlexRudaya <alexandra.rudaya88@gmail.com>
2024-04-12 13:34:31 +0300

    update for the test

--- src/test/java/com/aston/testng/FactorialTest.java
+++ src/test/java/com/aston/testng/FactorialTest.java
@@ -5,26 +5,21 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertThrows;
-//import org.testng.annotations.Test;
-//
-//import static org.testng.AssertJUnit.assertEquals;
-
 public class FactorialTest {
 
     SoftAssert softAssert = new SoftAssert();
+
     @Test
     public void possitiveNumbers() {
         Factorial positive = new Factorial();
-                softAssert.assertEquals(120, positive.getFactorial(5));
-                softAssert.assertEquals(1, positive.getFactorial(1));
-                softAssert.assertEquals(1, positive.getFactorial(0));
-                softAssert.assertEquals(738197504,positive.getFactorial(31));
-                softAssert.assertAll();
+        softAssert.assertEquals(120, positive.getFactorial(5));
+        softAssert.assertEquals(1, positive.getFactorial(1));
+        softAssert.assertEquals(1, positive.getFactorial(0));
+        softAssert.assertEquals(738197504, positive.getFactorial(31));
+        softAssert.assertAll();
     }
 
-  //  @Test(expectedExceptions = IllegalArgumentException.class)
+    //  @Test(expectedExceptions = IllegalArgumentException.class)
     @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Number can't be negative")
     public void negativeNumber() {
         Factorial negative = new Factorial();
