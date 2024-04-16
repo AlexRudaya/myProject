fcff7889e1a032a39626ad7a43e857b98f0ca4c2
AlexRudaya <alexandra.rudaya88@gmail.com>
2024-04-16 18:16:17 +0300

    updated

--- src/main/java/com/aston/Lesson_6/Phonebook.java
+++ src/main/java/com/aston/Lesson_6/Phonebook.java
@@ -4,14 +4,14 @@ package com.aston.Lesson_6;
 public class Phonebook {
     public static void main(String[] args) {
         Records records = new Records();
-        records.add("11111", "Ivanov");
-        records.add("22222", "Petrov");
-        records.add("333333", "Petrov");
-        records.add("333333", "Smirnof");
-        records.add("444444", "Ivanov");
-        records.add("555555", "Petrov");
-        records.add("666666", "Petrov");
-        records.add("111111", "Smirnof");
+        records.add("1111111", "Ivanov");
+        records.add("2222222", "Petrov");
+        records.add("3333333", "Petrov");
+        records.add("4444444", "Smirnof");
+        records.add("5555555", "Ivanov");
+        records.add("6666666", "Petrov");
+        records.add("7777777", "Petrov");
+        records.add("8888888", "Smirnof");
 
         System.out.println(records.get("Petrov"));
     }
