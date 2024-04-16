1d2291080a1019fc8585e8c423d7b9c3da9e7209
AlexRudaya <alexandra.rudaya88@gmail.com>
2024-04-16 15:07:29 +0300

    update for Phonebook

--- src/main/java/com/aston/Lesson_6/Phonebook.java
+++ src/main/java/com/aston/Lesson_6/Phonebook.java
@@ -1,18 +1,18 @@
 package com.aston.Lesson_6;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Phonebook {
     public static void main(String[] args) {
         Records records = new Records();
-        records.add("Ivanov", "33333");
-        records.add("Petrov", "222222");
-        records.add("Ivanov", "111111");
-        records.add("Stepanov", "222222");
-        records.add("Ivanov", "111111");
-        records.add("Ivanov", "555555");
+        records.add("11111", "Ivanov");
+        records.add("22222", "Petrov");
+        records.add("333333", "Petrov");
+        records.add("333333", "Smirnof");
+        records.add("444444", "Ivanov");
+        records.add("555555", "Petrov");
+        records.add("666666", "Petrov");
+        records.add("111111", "Smirnof");
 
-        System.out.println(records.get("Ivanov"));
+        System.out.println(records.get("Petrov"));
     }
 }
--- src/main/java/com/aston/Lesson_6/Records.java
+++ src/main/java/com/aston/Lesson_6/Records.java
@@ -3,22 +3,43 @@ package com.aston.Lesson_6;
 import java.util.*;
 
 public class Records {
-    private final HashMap<String, Set<String>> record;
+    //для наглядности первой реализации оставлю эту часть закоменченной
+//    private final HashMap<String, Set<String>> record;
+
+//    public Records() {
+//        this.record = new HashMap<>();
+//    }
+//
+//    public void add(String name, String number) {
+//        if (record.get(name) == null) {
+//            record.put(name, new HashSet<>());
+//        }
+//        record.get(name).add(number);
+//
+//    }
+//
+//    public Set<String> get(String name) {
+//        return record.get (name);
+//    }
+
+    private final HashMap<String, String> record;
 
     public Records() {
         this.record = new HashMap<>();
     }
 
-    public void add(String name, String number) {
-        if (record.get(name) == null) {
-            record.put(name, new HashSet<>());
-        }
-        record.get(name).add(number);
-
+    public void add(String number, String name) {
+        record.put(number, name);
     }
 
-    public Set<String> get(String name) {
-        return record.get(name);
+    public List<String> get(String name) {
+        List<String> phones = new ArrayList<>();
+        for (Map.Entry<String, String> o : record.entrySet()) {
+            if (o.getValue().equals(name)) {
+                phones.add(o.getKey());
+            }
+        }
+        return phones;
     }
 
 }
